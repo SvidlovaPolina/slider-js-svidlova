a144a1845930025afd9b97265c26a3ab62534c80
Polina <polina5523312@gmail.com>
2023-03-04 16:23:11 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,147 @@
+let images = [{
+  url: "./images/admiral.png",
+  title: "Rostov-on-Don, Admiral"
+}, {
+  url: "./images/thieves.png",
+  title: "Sochi Thieves"
+}, {
+  url: "./images/patriotic.png",
+  title: "Rostov-on-Don Patriotic"
+}];
+
+function initSlider(options) {
+  if (!images || !images.length) return;
+  
+  options = options || {
+    titles: false,
+    dots: true,
+    autoplay: false,
+    navigation: false
+  };
+    
+  let sliderImages = document.querySelector(".slider__images");
+  let sliderArrows = document.querySelector(".slider__arrows");
+  let sliderDots = document.querySelector(".slider__dots");
+  let sliderNavigation = document.querySelector(".slider__navigation");
+
+  initImages();
+  initArrows();
+
+  if (options.dots) {
+    initDots();
+  }
+
+  if (options.titles) {
+    initTitles();
+  }
+
+  if (options.autoplay) {
+    initAutoplay();
+  }
+  
+  if (options.navigation) {
+    initNavigation(); 
+  }
+
+  function initImages() {
+    images.forEach((image, index) => {
+      let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+      sliderImages.innerHTML += imageDiv;
+    });
+  }
+
+  function initArrows() {
+    sliderArrows.querySelectorAll(".slider__arrow").forEach(arrow => {
+      arrow.addEventListener("click", function() {
+        let curNumber = +sliderImages.querySelector(".active").dataset.index;
+        let nextNumber;
+        if (arrow.classList.contains("left")) {
+          nextNumber = curNumber === 0? images.length - 1 : curNumber -1;
+        } else {
+            nextNumber = curNumber === images.length - 1? 0 : curNumber +1;
+        }
+        moveSlider(nextNumber);
+      });
+   });
+  }
+
+  function initDots() {
+    images.forEach((image, index) => {
+      let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+      sliderDots.innerHTML += dot;
+    });
+    sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
+      dot.addEventListener("click", function() {
+        moveSlider(this.dataset.index);
+      })
+    })
+  }
+  
+  function initNavigation() {
+    images.forEach((image, index) => {
+      let nav = `<li class="slider__navigation-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].title}</li>`;
+      sliderNavigation.innerHTML += nav;
+    });
+    sliderNavigation.querySelectorAll(".slider__navigation-item").forEach(nav => {
+      nav.addEventListener("click", function() {
+        moveSlider(this.dataset.index);
+      })
+    })
+  }
+
+  function moveSlider(num) {
+    sliderImages.querySelector(".active").classList.remove("active");
+    sliderImages.querySelector(".n" + num).classList.add("active");
+    
+    if (options.dots) {
+      sliderDots.querySelector(".active").classList.remove("active");
+      sliderDots.querySelector(".n" + num).classList.add("active");
+    }
+      
+   if (options.navigation) {
+        sliderNavigation.querySelector(".active").classList.remove("active");
+        sliderNavigation.querySelector(".n" + num).classList.add("active");
+    }
+
+    if (options.titles) changeTitle(num);
+  }
+
+  function initTitles() {
+    let titleDiv = `<div class="slider__images-title">${images[0].title}</div>`;
+    sliderImages.innerHTML += cropTitle(titleDiv, 50);
+  }
+
+  function changeTitle(num) {
+    if (!images[num].title) return;
+    let sliderTitle = sliderImages.querySelector(".slider__images-title");
+    sliderTitle.innerText = cropTitle(images[num].title, 50);
+  }
+
+  function cropTitle(title, size) {
+    if (title.length <= size) {
+      return title;
+    } else {
+      return title.substr(0, size) + "...";
+    }
+  }
+
+  function initAutoplay() {
+    setInterval(() => {
+      let curNumber = +sliderImages.querySelector(".active").dataset.index;
+      let nextNumber = curNumber === images.length -1? 0 : curNumber +1;
+      moveSlider(nextNumber);
+    }, options.autoplayInterval);
+  }
+}
+
+let sliderOptions = {
+  dots: true,
+  titles: false,
+  autoplay: true,
+  autoplayInterval: 5000,
+  navigation: true
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  initSlider(sliderOptions);
+});
